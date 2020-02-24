11d050c67ac8921b5470a9926d1056896d30e8ff
Darrell-Conklin <dmconklin84@gmail.com>
2020-02-23 22:30:07 -0600

    Added carousel functionality

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,77 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+let createCarousel = () => {
+
+  let carousel = document.createElement('div');
+  carousel.setAttribute('class', 'carousel');
+
+  let leftButton = document.createElement('div');
+  leftButton.setAttribute('class', 'left-button');
+  leftButton.textContent = ' < ';
+
+  let imgOne = document.createElement('img');
+  imgOne.setAttribute('src', './assets/carousel/mountains.jpeg');
+
+  let imgTwo = document.createElement('img');
+  imgTwo.setAttribute('src', './assets/carousel/computer.jpeg');
+
+  let imgThree = document.createElement('img');
+  imgThree.setAttribute('src', './assets/carousel/trees.jpeg');
+
+  let imgFour = document.createElement('img');
+  imgFour.setAttribute('src', './assets/carousel/turntable.jpeg');
+
+  let rightButton = document.createElement('div');
+  rightButton.setAttribute('class', 'right-button');
+  rightButton.textContent = ' > ';
+
+  carousel.appendChild(leftButton);
+  carousel.appendChild(imgOne);
+  carousel.appendChild(imgTwo);
+  carousel.appendChild(imgThree);
+  carousel.appendChild(imgFour);
+  carousel.appendChild(rightButton);
+
+  return carousel;
+}
+
+let carouselContainer = document.querySelector('.carousel-container');
+carouselContainer.appendChild(createCarousel());
+
+let next = document.querySelector('.carousel div:nth-child(1)');
+let prev = document.querySelector('.carousel div:nth-child(6)');
+
+let carouselImages = document.querySelectorAll('.carousel img');
+
+let counter = 0;
+
+let amount = carouselImages.length;
+
+let current = carouselImages[0];
+
+current.style.display = 'block';
+
+function navigate(direction) {
+  current.style.display = 'none';
+  counter += direction;
+
+  if (direction === -1 && counter < 0) {
+    counter = amount -1;
+  }
+  if (direction === 1 && !items[counter]) {
+    counter = 0;
+  }
+
+  current = carouselImages[counter];
+  current.style.display = 'block';
+}
+
+next.addEventListener('click', () => {
+  navigate(1);
+});
+prev.addEventListener('click', () => {
+  navigate(-1);
+});
+
