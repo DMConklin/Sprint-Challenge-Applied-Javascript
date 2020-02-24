0840a2f1a3dcb74ba7ba84f98d5653f5f5d12113
Darrell-Conklin <dmconklin84@gmail.com>
2020-02-23 20:55:02 -0600

    fixed my confusion

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -10,46 +10,12 @@
 
 let createTopic = (topic) => {
     let newTab = document.createElement('div');
-    if (topic === 'node.js') {
-        newTab.setAttribute('class', `tab node`);
-    } else {
-        newTab.setAttribute('class', `tab ${topic}`);
-    }
+        
+    newTab.setAttribute('class', 'tab');
+    
     newTab.textContent = topic;
 
     return newTab;
-}
-
-let createArticle = (article) => {
-
-    let card = document.createElement('div');
-    card.setAttribute('class', 'card');
-
-    let headline = document.createElement('div');
-    headline.setAttribute('class', 'headline');
-    headline.textContent = article.headline;
-
-    let author = document.createElement('div');
-    author.setAttribute('class', 'author');
-
-    let imgContain = document.createElement('div');
-    imgContain.setAttribute('class', 'img-container');
-
-    let image = document.createElement('img');
-    image.setAttribute('src', article.authorPhoto);
-
-    let byAuthor = document.createElement('span');
-    byAuthor.textContent = article.authorName;
-
-    card.appendChild(headline);
-    card.appendChild(author);
-
-    author.appendChild(imgContain);
-    author.appendChild(byAuthor)
-
-    imgContain.appendChild(image);
-
-    return card;
 }
 
 let topicsContainer = document.querySelector('.topics');
@@ -59,18 +25,6 @@ axios.get('https://lambda-times-backend.herokuapp.com/topics').
         topics.data.topics.forEach(topic => {
             topicsContainer.appendChild(createTopic(topic));
         })
-        axios.get('https://lambda-times-backend.herokuapp.com/articles').
-            then(articles => {
-                console.log(articles);
-                Object.keys(articles.data.articles).forEach(articleTopic => {
-                    articles.data.articles[articleTopic].forEach(value => {
-                        document.querySelector(`.${articleTopic}`).appendChild(createArticle(value));
-                    })
-                })
-            }).
-            catch(err => {
-                console.log(err);
-            })
     }).
     catch(err => {
         console.log(err);
